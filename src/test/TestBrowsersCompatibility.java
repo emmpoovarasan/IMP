972931945db8d41abf13ee2b15ca14feff2f3271
poovarasan <poovarsan@easymedmobile.com>
2014-01-20 17:11:17 +0530

    updated code in TestBrowsersCompatibility.java and config.properties

--- src/test/TestBrowsersCompatibility.java
+++ src/test/TestBrowsersCompatibility.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.Arrays;
+
 import keyworddriven.UtilFunctions.DefinedValuesFromGetProperties;
 
 import org.junit.Test;
@@ -7,31 +9,52 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBrowsersCompatibility {
 
-	private WebDriver _driver;
+	private WebDriver driver;
 	
 	@Test
 	public void IEConfiguration() throws Exception{
 		System.setProperty("webdriver.ie.driver",DefinedValuesFromGetProperties.getDriverName("IE32bit"));
-		_driver = new InternetExplorerDriver();
+		driver = new InternetExplorerDriver();
 		TestBrowser();
 	}
 	@Test
 	public void FFConfiguration(){
-		_driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
 		TestBrowser();
 	}
 	@Test
 	public void GCConfiguration(){
 		System.setProperty("webdriver.chrome.driver",DefinedValuesFromGetProperties.getDriverName("GoogleChrome"));
-		_driver = new ChromeDriver();
+		
+		
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+
+		String path;
+		
+		if (System.getProperty("os.name").toUpperCase().contains("XP")) {
+		path = "C:\\Documents and Settings\\"
+		+ System.getProperty("user.name")
+		+ "\\Local Settings\\Application Data\\Google\\Chrome\\User Data";
+		} else {
+		path = "C:\\Users\\"
+		+ System.getProperty("user.name")
+		+ "\\AppData\\Local\\Google\\Chrome\\User Data";
+		}
+		capabilities.setCapability("chrome.switches", Arrays.asList("--user-data-dir="+path));
+		driver = new ChromeDriver(capabilities); 
+		
+		//driver = new ChromeDriver();
 		TestBrowser();
 	}
 	
 	public void TestBrowser(){
-		_driver.get("http://www.google.com");
-		System.out.println(_driver.getTitle().toString());
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle().toString());
+		driver.close();
 	}
+	
 }
